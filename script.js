ba0cb1bad63159523ae706a18cff1093053f41a9
Hannan Satopay <sathannan@hotmail.com>
2021-06-19 15:23:27 +0530

    home

---
+++ script.js
@@ -0,0 +1,83 @@
+new Swiper('.hero', {
+  loop: false,
+  slidesPerView: 1
+});
+
+new Swiper('.featured', {
+  loop: false,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    640: {
+      slidesPerView: 5,
+      spaceBetween: 40
+    }
+  }
+});
+
+new Swiper('.economy', {
+  loop: false,
+  slidesPerView: 2.5,
+  spaceBetween: 10,
+  breakpoints: {
+    640: {
+      slidesPerView: 5,
+      spaceBetween: 10
+    }
+  }
+});
+
+new Swiper('.news', {
+  loop: false,
+  slidesPerView: 2.5,
+  spaceBetween: 10,
+  breakpoints: {
+    640: {
+      slidesPerView: 5,
+      spaceBetween: 20
+    }
+  }
+});
+
+new Swiper('.testimonials', {
+  loop: false,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  slidesPerView: 1.4,
+  spaceBetween: 10,
+  breakpoints: {
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 10
+    }
+  }
+});
+
+let mybutton = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
